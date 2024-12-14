b91dcce5ec6c3f94119f0bd1a7408b63035af03f
Vlad Rassokha <vladislavrassokha@gmail.com>
2024-12-14 09:28:50 +0300

    Last fix settings.py and edited readme

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 ## requirements:
 
+Создать venv
+
 `pip install -r requirements.txt` - установить необходимые зависимости
 
 ## подключение бд:
 
 `python manage.py makemigrations first_project`
+
 `python manage.py migrate first_project`
 
--- first_project/settings.py
+++ first_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+l!yu&#^+^m0qek^1@ujk4g^w4rd6_7*omuy7w%p6tnm4xh=1+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
